1fb6ba1b9de7992fb44cada9e820a00250da4d46
Jacelynk <carataojacelyn145@gmail.com>
2025-06-20 14:40:01 +0800

    Updated landing page
    correct measurements and positions

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './styles/global.css';
 import Navbar from './components/Navbar';
 import './App.css';
 import RevealCircle from './components/RevealCircle';
-import bgImg from './assets/img/JACELYN A. CARATAO 1.png';
+import bgImg from './assets/img/bago.png';
 import nameImg from './assets/img/JACELYN A. CARATAO.png';
 
 function App() {
@@ -10,24 +10,26 @@ function App() {
     <div
       className="App"
       style={{
-        minHeight: '100vh',
-        width: '100vw',
-        background: `url(${bgImg}) center/cover no-repeat fixed`,
+        minHeight: '105vh',
+        width: '99vw',
+        background: `url(${bgImg}) center center/cover no-repeat fixed`,
         position: 'relative',
         overflow: 'hidden',
       }}
     >
       <Navbar />
-      <RevealCircle />
-      <div style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        marginTop: '1.5rem',
-        position: 'relative',
-        zIndex: 2
-      }}>
-        <img src={nameImg} alt="Jacelyn A. Caratao" style={{ maxWidth: '90vw', height: 'auto' }} />
+      <div style={{ marginLeft: '3rem', marginTop: '-1rem' }}>
+        <RevealCircle />
+        <div style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          marginTop: '1.5rem',
+          position: 'relative',
+          zIndex: 2
+        }}>
+          <img src={nameImg} alt="Jacelyn A. Caratao" style={{ maxWidth: '90vw', height: 'auto' }} />
+        </div>
       </div>
     </div>
   );
